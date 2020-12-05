d02a9986f1b2d224807f6fd68c90371ee9d9e216
Shilpa-K07 <shilpa07udupi@gmail.com>
2020-12-05 22:08:31 +0530

    feat: implement fetch api fot get request

--- Greeting-App-Front-End/js/index.js
+++ Greeting-App-Front-End/js/index.js
@@ -16,12 +16,29 @@ edit = () => {
     document.getElementById("message").innerHTML = "Hi"
 }
 get = () => {
+    debugger;
     fetch('http://localhost:3000/greetings/')
-    .then(
-    response => response.json())
-    .then(people => {
-    const names = data.map(data => data.name).join("\n");
-    console.log(names)
-    })
+        .then(response => response.json())
+        .then(data => createElements(data))
+        .catch(err => console.log(err))
 }
 
+createElements = (data) => {
+    var index = 0;
+    const mainDiv = document.querySelector('#main');
+    for(const q of data.data) {
+        const firstDiv = document.createElement('div');
+        const firstPara = document.createElement('p');
+        const secondPara = document.createElement('p');
+        const thirdPara = document.createElement('p');
+        firstDiv.className = "content"
+
+        firstPara.innerHTML = data.data[index]._id;
+        secondPara.innerHTML = data.data[index].name;
+        thirdPara.innerHTML = data.data[index].message;
+        index++;
+
+        firstDiv.append(firstPara,secondPara,thirdPara);
+        mainDiv.append(firstDiv);
+    }
+}
