16ecb46b7a8abdb4fa6ee50cd926a20fa1ceec03
Shilpa-K07 <shilpa07udupi@gmail.com>
2020-12-06 21:13:50 +0530

    feat: add fetch api for get and post request

--- Greeting-App-Front-End/js/index.js
+++ Greeting-App-Front-End/js/index.js
@@ -9,14 +9,14 @@ remove = () => {
     document.getElementById("list3").style.display = 'none'
 }
 add = () => {
-    document.getElementById("list1").style.display = 'block'
-    document.getElementById("message").innerHTML = "Hello"
+    document.getElementById('add').style.display = 'block'
+    document.getElementById('main').style.display = 'none'
 }
 edit = () => {
     document.getElementById("message").innerHTML = "Hi"
 }
-get = () => {
-    debugger;
+get = () => {debugger;
+    document.getElementById('add').style.display = 'none';
     fetch('http://localhost:3000/greetings/')
         .then(response => response.json())
         .then(data => createElements(data))
@@ -40,5 +40,25 @@ createElements = (data) => {
 
         firstDiv.append(firstPara,secondPara,thirdPara);
         mainDiv.append(firstDiv);
+        mainDiv.style.display = 'block'
     }
 }
+   /*  addData = () => {debugger;
+        name = document.getElementById("name").value;
+        message = document.getElementById("message").value;
+        fetch('http://localhost:3000/greetings/', {
+            method: 'POST',
+            headers: {
+            'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({name: name, message: message}),
+        })
+        .then(response => response.json())
+        .then(data => {
+            console.log('Success:', data);
+        })
+        .catch((error) => {
+            console.error('Error:', error);
+        });
+    }
+} */
